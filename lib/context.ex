f9c1a165c1581b8cb291523960e3a66cdb920794
Szymon Soppa <szymon@curiosum.com>
2023-04-03 15:39:54 +0200

    Adds Github CI template

--- lib/context.ex
+++ lib/context.ex
@@ -1,2 +1,3 @@
 defmodule Context do
+  @moduledoc false
 end
--- mix.exs
+++ mix.exs
@@ -20,6 +20,6 @@ defmodule Context.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [{:credo, "~> 1.7", only: [:dev, :test], runtime: false}]
   end
 end
