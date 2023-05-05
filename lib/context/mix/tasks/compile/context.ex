b6f23ddb53436858e8964cf4e93c2500dbfd75ab
Szymon Soppa <szymon@curiosum.com>
2023-05-05 15:43:32 +0200

    Enable recompilation via config

--- lib/context/mix/tasks/compile/context.ex
+++ lib/context/mix/tasks/compile/context.ex
@@ -11,9 +11,16 @@ defmodule Mix.Tasks.Compile.Context do
   Sets the custom compiler tracer to the Context.Tracer module.
   """
   def run(_argv) do
-    Mix.Task.Compiler.after_compiler(:app, &Tracer.after_compiler/1)
+    if recompilation_enabled?() do
+      Mix.Task.Compiler.after_compiler(:app, &Tracer.after_compiler/1)
+    end
 
     tracers = Code.get_compiler_option(:tracers)
     Code.put_compiler_option(:tracers, [Tracer | tracers])
+  end
+
+  @spec recompilation_enabled? :: boolean()
+  defp recompilation_enabled? do
+    Application.get_env(:context, :enable_recompilation)
   end
 end
