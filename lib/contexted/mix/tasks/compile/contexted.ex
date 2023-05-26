668b8b683746aa371180b6f63d8d74d9f91bdcfe
Szymon Soppa <szymon@curiosum.com>
2023-05-26 20:05:32 +0200

    Don't run tracer and recompilation without contexts list

--- lib/contexted/mix/tasks/compile/contexted.ex
+++ lib/contexted/mix/tasks/compile/contexted.ex
@@ -8,17 +8,23 @@ defmodule Mix.Tasks.Compile.Contexted do
   A custom Elixir compiler task that checks for cross-references between specific modules, known as "contexts".
   """
 
+  @contexts Application.compile_env(:contexted, :contexts, [])
+
   @doc """
   Sets the custom compiler tracer to the Contexted.Tracer module.
   """
   @spec run(any()) :: :ok
   def run(_argv) do
-    if recompilation_enabled?() do
-      Compiler.after_compiler(:app, &Tracer.after_compiler/1)
-    end
+    if Enum.count(@contexts) > 0 do
+      if recompilation_enabled?() do
+        Compiler.after_compiler(:app, &Tracer.after_compiler/1)
+      end
 
-    tracers = Code.get_compiler_option(:tracers)
-    Code.put_compiler_option(:tracers, [Tracer | tracers])
+      tracers = Code.get_compiler_option(:tracers)
+      Code.put_compiler_option(:tracers, [Tracer | tracers])
+    else
+      :ok
+    end
   end
 
   @spec recompilation_enabled? :: boolean()
